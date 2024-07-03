c4b58ffb876e2e2f74757b148f3722ea6ad4bbf8
PrathamSatpute <prathamsatpute2002@gmail.com>
2024-07-03 19:34:22 +0530

    Initial commit

--- spotify_auth.py
+++ spotify_auth.py
@@ -3,8 +3,8 @@ from spotipy.oauth2 import SpotifyOAuth
 
 def get_auth_manager():
     return SpotifyOAuth(
-        client_id='14e04f6d28884dcbb4a5a431679516cf',
-        client_secret='62ccbe9d8bf54d19ab71dd8451ead4bb',
+        client_id='your_client_id',
+        client_secret='your_client_secret',
         redirect_uri='http://localhost:5000/callback',
         scope='user-top-read user-read-recently-played'
     )
